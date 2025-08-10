ed0a6f2162cac4b4a77cd2d688af1436a190f28e
Alkdess <yosoykam@outlook.com>
2025-08-10 00:19:26 -0500

    Nuevos cambios, Insert en tabla

--- backend/Listado.API/Models/Personas.cs
+++ backend/Listado.API/Models/Personas.cs
@@ -3,6 +3,6 @@ namespace Listado.API.Models{
     public int Id {get; set;}
     public string FullName {get; set;}
     public string Address {get; set;}
-    public string UpdateDate {get; set;}
+    public DateTime Update_Date {get; set;}
   }
 }
